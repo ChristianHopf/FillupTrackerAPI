60201a5877b48b0a0bbac70692576954be068eb5
christian-hopf <christian-hopf@cognizant.com>
2024-08-20 11:31:43 -0400

    Enabled CORS

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+// Enable CORS
+app.use(cors());
+
+// Use .env file
 require("dotenv").config();
 
+// Register routes
 const usersRoute = require("./routes/users");
 app.use("/users", usersRoute);
 
+// Not sure whether to have nothing for this
 app.get("/", (req, res) => {
   res.send("<h1>Hello, Express.js Server!</h1>");
 });
