a56c95afd785814f0ef02dd9be96cd8f9735d13c
christian-hopf <christian-hopf@cognizant.com>
2024-08-20 11:32:40 -0400

    Endpoints return data in formats easier to handle

--- routes/users.js
+++ routes/users.js
@@ -11,25 +11,64 @@ const router = express.Router();
 // https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v1
 router.get("/getachievements/:steamid", async (req, res, next) => {
   try {
-    const response = await fetch(
+    const playerAchievementsResponse = await fetch(
       `https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v1/?appid=570940&key=${process.env.API_KEY}&steamid=${req.params.steamid}`
     );
-    if (!response.ok) {
-      console.error(`Error: Received status ${response.status}`);
-      return res.status(response.status).json({
+    if (!playerAchievementsResponse.ok) {
+      console.error(
+        `Error: Received status ${playerAchievementsResponse.status}`
+      );
+      return res.status(playerAchievementsResponse.status).json({
         error: true,
-        message: `Failed to fetch data with status ${response.status}`,
+        message: `Failed to fetch data with status ${playerAchievementsResponse.status}`,
       });
     }
-    const data = await response.json();
-    console.log(data);
-    res.json(data);
+    const achievementsSchemaResponse = await fetch(
+      `https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?appid=570940&key=${process.env.API_KEY}`
+    );
+    if (!achievementsSchemaResponse.ok) {
+      console.error(
+        `Error: Received status ${achievementsSchemaResponse.status}`
+      );
+      return res.status(achievementsSchemaResponse.status).json({
+        error: true,
+        message: `Failed to fetch data with status ${achievementsSchemaResponse.status}`,
+      });
+    }
+    const achievements = await playerAchievementsResponse.json();
+    const schema = await achievementsSchemaResponse.json();
+
+    // For each achievement:
+    // - if its entry in achievements is achieved
+    // - build a new object with its desired schema info
+    // - push to result
+    result = [];
+    schema.game.availableGameStats.achievements.forEach(function (
+      achievement,
+      index
+    ) {
+      let playerAchievement = achievements.playerstats.achievements[index];
+      if (playerAchievement.achieved) {
+        let resultAchievement = {
+          name: achievement.name,
+          displayName: achievement.displayName,
+          description: achievement.description,
+          unlocktime: playerAchievement.unlocktime,
+          icon: achievement.icon,
+        };
+        result.push(resultAchievement);
+      }
+    });
+    // console.log(result[1].unlocktime > result[2].unlocktime);
+    result.sort(({ unlocktime: a }, { unlocktime: b }) => b - a);
+    // console.log(result);
+    res.json(result);
   } catch (err) {
     next(err);
   }
 });
 
-router.get("/getplayersummaries/:steamid", async (req, res, next) => {
+router.get("/getprofile/:steamid", async (req, res, next) => {
   try {
     const response = await fetch(
       `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${process.env.API_KEY}&steamids=[${req.params.steamid}]`
@@ -43,7 +82,10 @@ router.get("/getplayersummaries/:steamid", async (req, res, next) => {
     }
     const data = await response.json();
     console.log(data);
-    res.json(data);
+    res.json({
+      personaname: data.response.players[0].personaname,
+      avatarmedium: data.response.players[0].avatarmedium,
+    });
   } catch (err) {
     next(err);
   }
@@ -64,7 +106,19 @@ router.get("/gethours/:steamid", async (req, res, next) => {
     }
     const data = await response.json();
     console.log(data);
-    res.json(data);
+
+    // Add case where response is empty (some kind of error on Steam's side?)
+
+    // res.json(data);
+    // If the user hasn't played the game in the last 2 weeks,
+    // the playtime_2weeks property is absent
+    data.response.games[0].playtime_2weeks ? res.json({
+      playtime_forever: data.response.games[0].playtime_forever,
+      playtime_2weeks: data.response.games[0].playtime_2weeks,
+    }) : res.json({
+      playtime_forever: data.response.games[0].playtime_forever,
+      playtime_2weeks: 0,
+    });
   } catch (err) {
     next(err);
   }
